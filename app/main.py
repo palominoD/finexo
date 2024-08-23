56ef5584c4ae70958f16d4af586d3f31bc825223
darted <castleemmu@gmail.com>
2024-08-23 17:17:38 -0600

    sistema flask completo

--- app/main.py
+++ app/main.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/service.html')
+def services():
+    return render_template('service.html')
+
+@app.route('/why.html')
+def why():
+    return render_template('why.html')
+
+@app.route('/team.html')
+def team():
+    return render_template('team.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
